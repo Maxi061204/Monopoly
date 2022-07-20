147095a38103345daa7f30c8d24bb26658c03972
Maxi061204 <kohnemaximilian@gmail.com>
2022-07-20 17:56:18 +0200

    s

--- Hintergrund.java
+++ Hintergrund.java
@@ -33,7 +33,22 @@ public class Hintergrund extends JFrame
                     {
                         ioe.printStackTrace();
                     }
-                    //Braun 1
+                    
+                    
+                   
+                    //Spieler 1
+                    try
+                    {
+                        Image bg = ImageIO.read(getClass().getResource("./spieler 1.png"));
+                        bg = bg.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bg,5,930,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    //brown 1
+                    //Ku1
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./brown 1.JPG"));
@@ -45,7 +60,8 @@ public class Hintergrund extends JFrame
                         ioe.printStackTrace();
                     }
                     
-                    //Braun 1
+                    //brown 2
+                    //Ku2
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./brown 2.JPG"));
@@ -58,6 +74,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //teal 1
+                    //Chemie 1
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./teal 1.JPG"));
@@ -70,8 +87,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //teal 2
-                    
-                    //teal 1
+                    //Chemie 2
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./teal 2.JPG"));
@@ -84,6 +100,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //teal 3
+                    //Chemie Übung
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./teal 3.JPG"));
@@ -96,6 +113,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //purple 1
+                    //Bio 1
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./purple 1.JPG"));
@@ -108,6 +126,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //purple 2
+                    // Bio 2
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./purple 2.JPG"));
@@ -120,6 +139,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //purple 3
+                    // Bio Übung
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./purple 3.JPG"));
@@ -132,6 +152,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //orange 1
+                    //Goethestrasse
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./orange 1.JPG"));
@@ -144,6 +165,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //orange 2
+                    // Uhlandstrasse
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./orange 2.JPG"));
@@ -156,6 +178,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //orange 3
+                    // Lessingstrasse
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./orange 3.JPG"));
@@ -168,6 +191,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //red 1
+                    // Donaupark
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./red 1.JPG"));
@@ -180,6 +204,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //red 2
+                    // Jahninsel
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./red 2.JPG"));
@@ -192,6 +217,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //red 3
+                    // Stadtpark
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./red 3.JPG"));
@@ -204,6 +230,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //yellow 1
+                    // K08
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./yellow 1.JPG"));
@@ -216,6 +243,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //yellow 2
+                    // K09
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./yellow 2.JPG"));
@@ -228,6 +256,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //yellow 3
+                    // K10
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./yellow 3.JPG"));
@@ -240,6 +269,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //green 1
+                    // Lehrmittelbibliothek
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./green 1.JPG"));
@@ -252,6 +282,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //green 2
+                    // Pausenverkauf
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./green 2.JPG"));
@@ -264,6 +295,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //green 3
+                    // Aula
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./green 3.JPG"));
@@ -276,6 +308,7 @@ public class Hintergrund extends JFrame
                     }
                     
                     //blue 1
+                    // Sekretariat
                     try
                     {
                         Image bag = ImageIO.read(getClass().getResource("./blue 1.JPG"));
@@ -288,9 +321,10 @@ public class Hintergrund extends JFrame
                     }
                     
                     //blue 2
+                    // Direktorat
                     try
                     {
-                        Image bag = ImageIO.read(getClass().getResource("./blue 1.JPG"));
+                        Image bag = ImageIO.read(getClass().getResource("./blue 2.JPG"));
                         bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
                         g.drawImage(bag,1180 ,700,this);
                     }
@@ -298,7 +332,85 @@ public class Hintergrund extends JFrame
                     {
                         ioe.printStackTrace();
                     }
+                    
+                    //train 1
+                    //DEZ
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./train 1.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1020,800,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    //train 2
+                    //KÖWE
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./train 2.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1020,850,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    
+                    //train 3
+                    //REZ
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./train 3.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1020,900,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    
+                    //train 4
+                    //Arcaden
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./train 4.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1020,950,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    
+                    //E Werk
+                    // Zink
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./E Werk.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1180 ,850,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
+                    
+                    //W Werk
+                    // Zink
+                    try
+                    {
+                        Image bag = ImageIO.read(getClass().getResource("./W Werk.jpeg"));
+                        bag = bag.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
+                        g.drawImage(bag,1180 ,900,this);
+                    }
+                    catch (java.io.IOException ioe)
+                    {
+                        ioe.printStackTrace();
+                    }
                 };
+                
             };
             this.setLayout(null);
             hintergrund.setLocation(5,5);
